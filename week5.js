7c766e2112d08f5123c2b1cd237b0d910d77e08b
maxestwell <maxwellpcorcoran@gmail.com>
2022-09-25 22:09:03 +1000

    update

--- week5.js
+++ week5.js
@@ -1,6 +1,11 @@
 /*
     Paste the code for your week 5 exercise below.
 */
+let circleX = 0;
+let circleY = 0;
+let xSpeed = 1;
+let ySpeed = 3;
+
 function setup() {
   if(windowWidth > windowHeight){
     createCanvas (windowHeight, windowHeight)
@@ -10,7 +15,33 @@ function setup() {
 }
 
 function draw() {
-  translate(width/2, height/2);
-  //scale(1, -1);
-  background('#B1CCC1');
+  // no background so the animation keeps all frames
+  frameRate(24);
+  textFont('Helvetica');
+  
+  square(circleX, circleY, 40);
+  text(' error', circleX, circleY, 40)
+
+  circleX = circleX + random(-2, 2);
+  circleY = circleY + random(-2, 2);
+  
+  circleX = circleX + xSpeed;
+  circleY = circleY + ySpeed;
+
+  
+  if(circleX < 0) {
+    xSpeed = xSpeed * -1;
+  }
+
+  if(circleX > width) {
+    xSpeed = xSpeed * -1;
+  }
+
+  if(circleY < 0) {
+    ySpeed = ySpeed * -1;
+  }
+
+  if(circleY > width) {
+    ySpeed = ySpeed * -1;
+  }
 }
--- week6.js
+++ week6.js
@@ -1,3 +1,39 @@
 /*
     Paste the code for your week 3 exercise below.
 */
+function setup() {
+  if(windowWidth > windowHeight){
+    createCanvas (windowHeight, windowHeight)
+  }else{
+    createCanvas(windowWidth, windowWidth)
+  }  
+}
+  
+function draw() {
+  background('#B1CCC1');
+    
+  push();
+  noFill();
+  stroke(0, 60);
+  strokeCap(ROUND);
+  strokeWeight(5);
+  circle(mouseX-50, mouseY-50, mouseY);
+  circle(mouseX+50, mouseY+50, mouseY);
+  circle(mouseX/1.25, mouseY*1.25, mouseY);
+  circle(mouseX*1.25, mouseY/1.25, mouseY);
+  pop();
+  
+  noStroke();
+  circle(mouseX, mouseY, mouseY+40);
+
+  push();
+  strokeCap(ROUND);
+  stroke('#222222');
+  strokeWeight(15);
+
+  point(mouseX-50, mouseY-50);
+  point(mouseX+50, mouseY+50);
+  point(mouseX/1.25, mouseY*1.25);
+  point(mouseX*1.25, mouseY/1.25);
+  pop();
+}
